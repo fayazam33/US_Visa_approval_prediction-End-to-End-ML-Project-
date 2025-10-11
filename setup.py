f3a6c6c72a1daa9a808de0f9f8eed2b8ce3515ff
Fayaz Ali Muktadir <121030621+fayazam33@users.noreply.github.com>
2025-10-12 01:58:40 +0600

    adding requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages
+
+setup(
+    name="us_visa_approval",
+    version="0.0.0",
+    author="Fayaz",
+    author_email="2000famuktadir@gmail.com",
+    packages=find_packages()
+)
